72555716424b40e0382d1cc9bfb6d3bb5fd884d4
Anthony Chalabis <anthony.chalabis01@gmail.com>
2025-05-01 18:54:20 -0500

    Attempt

--- fs_project/settings.py
+++ fs_project/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['family-schedule.onrender.com']
+ALLOWED_HOSTS = [
+    'family-schedule.onrender.com',
+                 ]
 
 
 # Application definition
@@ -115,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
