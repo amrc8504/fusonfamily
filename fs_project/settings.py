d82b562484b14526e3a9ffc32694f8af39ebdda1
Anthony Chalabis <anthony.chalabis01@gmail.com>
2025-05-01 20:34:05 -0500

    Created fallback for Cloudinary

--- fs_project/settings.py
+++ fs_project/settings.py
@@ -33,9 +33,9 @@ ALLOWED_HOSTS = [
 
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': config('CLOUDINARY_API_KEY'),
-    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME', default=''),
+    'API_KEY': config('CLOUDINARY_API_KEY', default=''),
+    'API_SECRET': config('CLOUDINARY_API_SECRET', default=''),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Application definition
