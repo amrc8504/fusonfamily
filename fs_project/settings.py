d8400e9df42f82560177f96b34190575b8c13125
Anthony Chalabis <anthony.chalabis01@gmail.com>
2025-05-01 20:25:58 -0500

    Added cloud storage to settings for imgs

--- fs_project/settings.py
+++ fs_project/settings.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 from decouple import config
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +32,12 @@ ALLOWED_HOSTS = [
                  ]
 
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'schedules',
     'announcements',
-
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
