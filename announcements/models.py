f4a1a4842f89c158129b93f0596c17565879d357
Anthony Chalabis <anthony.chalabis01@gmail.com>
2025-05-01 20:28:49 -0500

    Forgot to adjust model for Cloudinary

--- announcements/models.py
+++ announcements/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Announcement(models.Model):
     title = models.CharField(max_length=200)
     message = models.TextField()
-    image = models.ImageField(upload_to='announcement_images/', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
